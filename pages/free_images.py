5a80a0fd81a77da88d7d7b2ed34063c748d5da2d
Magwit <magnushakon@hotmail.com>
2020-05-23 11:39:43 +0200

    create basic stubs for page objects

---
+++ pages/free_images.py
@@ -0,0 +1,18 @@
+class FreeImages:
+    def __init__(self, browser):
+        self.browser = browser
+
+    # action methods
+
+    def scroll_to_wikimedia_tile(self):
+        # TODO
+        pass
+
+    def click_wikimedia_tile(self):
+        # TODO
+        pass
+
+    # return methods
+    def title():
+        # TODO
+        pass
---
+++ pages/homepage.py
@@ -0,0 +1,13 @@
+# page objects can have actions and also return values
+# A
+class HomePage:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def scroll_to_free_images_tile(self):
+        # TODO
+        pass
+
+    def click_on_free_images_tile(self):
+        # TODO
+        pass
