d7039ae4bb14b0111a362797e6c0348013477bb7
Magwit <magnushakon@hotmail.com>
2020-05-25 13:23:08 +0200

    add free images page object

--- pages/free_images.py
+++ pages/free_images.py
@@ -1,20 +1,39 @@
+from selenium.webdriver.common.by import By
+import time  # TODO remove sleep in final version
+
+
 class FreeImages:
+
+    # URL
+    URL = "https://www.nationalmuseum.se/samlingarna/fria-bilder"
+
+    # Locators
+    WIKIMEDIA_TILE = (
+        By.XPATH,
+        "//a[@href='https://commons.wikimedia.org/wiki/Category:Media_contributed_by_Nationalmuseum_Stockholm:_2016-10' and @class='link-container']",
+    )
+
+    # Initializer and setting state
     def __init__(self, browser):
         self.browser = browser
-
-    # action methods
 
     def load(self):
         # TODO load is needed for the last test case
         pass
 
+    # Interaction methods
+
     def scroll_to_wikimedia_tile(self):
-        # TODO
-        pass
+        # Declaring an instance variable which will be re used in the click function
+        self.wikimedia_tile = self.browser.find_element(*self.WIKIMEDIA_TILE)
+        self.browser.execute_script(
+            "arguments[0].scrollIntoView({block: 'center'})", self.wikimedia_tile
+        )
 
     def click_wikimedia_tile(self):
-        # TODO
-        pass
+        self.browser.implicitly_wait(10)
+        self.browser.execute_script("arguments[0].click()", self.wikimedia_tile)
+        time.sleep(8)
 
     # return methods
     def title(self):
--- pages/homepage.py
+++ pages/homepage.py
@@ -1,9 +1,5 @@
-# page objects can have actions and also return values
-# A
 from selenium.webdriver.common.by import By
-import time
-
-# from selenium.webdriver.common.action_chains import ActionChains
+import time  # TODO remove sleep in final version
 
 
 class HomePage:
@@ -24,17 +20,18 @@ class HomePage:
     def load(self):
         self.browser.get(self.URL)
 
-    # Interaction modules
+    # Interaction methods
     def scroll_to_free_images_tile(self):
-
-        free_images_tile = self.browser.find_element(*self.FREE_IMAGES_TILE)
-
+        # using self to scope to class -> instance variable as the locator is re-used
+        # TODO ask if this is an antipattern or just quirky
+        # DONE https://qaboy.com/2018/01/22/test-automation-framework-with-python-part-2/
+        self.free_images_tile = self.browser.find_element(*self.FREE_IMAGES_TILE)
         self.browser.execute_script(
-            "arguments[0].scrollIntoView({block: 'center'})", free_images_tile
+            "arguments[0].scrollIntoView({block: 'center'})", self.free_images_tile
         )
 
     def click_on_free_images_tile(self):
         self.browser.implicitly_wait(10)
-        free_images_tile = self.browser.find_element(*self.FREE_IMAGES_TILE)
-        self.browser.execute_script("arguments[0].click()", free_images_tile)
+        # free_images_tile = self.browser.find_element(*self.FREE_IMAGES_TILE)
+        self.browser.execute_script("arguments[0].click()", self.free_images_tile)
         time.sleep(8)
